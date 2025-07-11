0cb31086f9d3e4466e5d9dcb23537770ae178581
Libny Pacheco <libnypacheco@gmail.com>
2025-07-11 09:29:59 +0200

    feat: wrap App component in React.StrictMode for improved development checks

--- my-sites/src/company/main.tsx
+++ my-sites/src/company/main.tsx
@@ -4,4 +4,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "../styles/globals.css";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")!).render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
