83a9c3267176c1dc630eb83cfc5cb510f72444cd
Libny Pacheco <libnypacheco@gmail.com>
2025-07-11 09:47:18 +0200

    refactor: clean up vite.config.ts and update package.json scripts

--- my-sites/vite.config.ts
+++ my-sites/vite.config.ts
@@ -1,15 +1,9 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { resolve } from "path";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: {
-        index: resolve(__dirname, "public/index.html"),
-        tool-01: resolve(__dirname, "public/tool-01.html"),
-      },
-    },
+  server: {
+    open: true,
   },
 });
