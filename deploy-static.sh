9a289cc29a401e3ee85fb12b23df4d901112154b
STASYA00 <0.0stasya@gmail.com>
2025-07-25 22:42:39 +0200

    feat: add scaleway deployment files

---
+++ deploy-static.sh
@@ -0,0 +1,3 @@
+docker build -t rg.fr-par.scw.cloud/web_upskiller/daylight-factor:latest .
+docker push rg.fr-par.scw.cloud/web_upskiller/daylight-factor:latest
+scw container container update a89877ae-a8f7-42fd-b3f9-dd6ad38ffad6
